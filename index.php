f3ac1a7918ca90acab0c92a4e112e806862efdbb
HGP45 <hgp45@THes-Macintosh.local>
2021-11-11 17:45:31 -0800

    use java script to hide navigation bar

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>VideoTube</title>
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> 
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="assets/js/CommonActions.js"></script>
+    </head>
+
+    <body>
+    
+
+        <div id="pageContainer">
+
+            <div id="mastHeadContainer">
+                <button class="navShowHide">Click me!</button>
+            </div>
+
+            <div id="sideNavContainer"style="display:none">
+
+            </div>
+
+            <div id="mainSectionContainer">
+
+                <div id="mainContentContainer">
+                    hello everyone
+                </div>
+        
+            </div> 
+    
+        </div>
+
+    </body>
+
+</html>
